793af62c747194a5744734d041033bdad813932d
JeisonR72 <jeisonramirezjara835@gmail.com>
2022-09-11 10:04:19 -0500

    agregado carrito

---
+++ products.js
@@ -0,0 +1,29 @@
+const products = [
+    {id: 1, name: "A1 Italia", cantidad: 1, price:50000, hora: "10 am",categoria:"francia", plataforma: "todas" , img: 'https://www.101viajes.com/sites/default/files/tren-francia.jpg'},
+    {id: 2, name: "A2 Italia", cantidad: 1, price:60000, hora: "12 pm",categoria:"francia", plataforma: "todas" , img: 'https://www.101viajes.com/sites/default/files/tren-francia.jpg'},
+    {id: 3, name: "A3 Italia", cantidad: 1, price:80000, hora: "2 pm",categoria:"francia", plataforma: "todas" , img: 'https://www.101viajes.com/sites/default/files/tren-francia.jpg'},
+    {id: 4, name: "A4 Italia", cantidad: 1, price:70000, hora: "4 pm",categoria:"francia", plataforma: "todas" , img: 'https://www.101viajes.com/sites/default/files/tren-francia.jpg'},
+    {id: 5, name: "A5 Italia", cantidad: 1, price:40000, hora: "6 pm",categoria:"francia", plataforma: "todas" , img: 'https://www.101viajes.com/sites/default/files/tren-francia.jpg'},
+    {id: 6, name: "A6 Italia", cantidad: 1, price:55000, hora: "8 pm",categoria:"francia", plataforma: "todas" , img: 'https://www.101viajes.com/sites/default/files/tren-francia.jpg'},
+    {id: 7, name: "B1 España", cantidad: 1, price:50000, hora: "10 am",categoria:"españa", plataforma: "todas" , img: 'https://www.truecalia.com/blog/wp-content/uploads/seguridad-de-los-trenes-en-Espa%C3%B1a-PORTADA.jpg'},
+    {id: 8, name: "B2 España", cantidad: 1, price:60000, hora: "12 pm",categoria:"españa", plataforma: "todas" , img: 'https://www.truecalia.com/blog/wp-content/uploads/seguridad-de-los-trenes-en-Espa%C3%B1a-PORTADA.jpg'},
+    {id: 9, name: "B3 España", cantidad: 1, price:80000, hora: "2 pm",categoria:"españa", plataforma: "todas" , img: 'https://www.truecalia.com/blog/wp-content/uploads/seguridad-de-los-trenes-en-Espa%C3%B1a-PORTADA.jpg'},
+    {id: 10, name: "B4 España", cantidad: 1, price:70000, hora: "4 pm",categoria:"españa", plataforma: "todas" , img: 'https://www.truecalia.com/blog/wp-content/uploads/seguridad-de-los-trenes-en-Espa%C3%B1a-PORTADA.jpg'},
+    {id: 11, name: "B5 España", cantidad: 1, price:40000, hora: "6 pm",categoria:"españa", plataforma: "todas" , img: 'https://www.truecalia.com/blog/wp-content/uploads/seguridad-de-los-trenes-en-Espa%C3%B1a-PORTADA.jpg'},
+    {id: 12, name: "B6 España", cantidad: 1, price:55000, hora: "8 pm",categoria:"españa", plataforma: "todas" , img: 'https://www.truecalia.com/blog/wp-content/uploads/seguridad-de-los-trenes-en-Espa%C3%B1a-PORTADA.jpg'},
+    {id: 13, name: "C1 Paris", cantidad: 1, price:50000, hora: "10 am",categoria:"paris", plataforma: "todas" , img: 'https://www.parisciudad.com/fotos/tren-larga-distancia.jpg'},
+    {id: 14, name: "C2 Paris", cantidad: 1, price:60000, hora: "12 pm",categoria:"paris", plataforma: "todas" , img: 'https://www.parisciudad.com/fotos/tren-larga-distancia.jpg'},
+    {id: 15, name: "C3 Paris", cantidad: 1, price:100000, hora: "2 pm",categoria:"paris", plataforma: "todas" , img: 'https://www.parisciudad.com/fotos/tren-larga-distancia.jpg'},
+    {id: 16, name: "C4 Paris", cantidad: 1, price:70000, hora: "4 pm",categoria:"paris", plataforma: "todas" , img: 'https://www.parisciudad.com/fotos/tren-larga-distancia.jpg'},
+    {id: 17, name: "C5 Paris", cantidad: 1, price:40000, hora: "6 pm",categoria:"paris", plataforma: "todas" , img: 'https://www.parisciudad.com/fotos/tren-larga-distancia.jpg'},
+    {id: 18, name: "C6 Paris", cantidad: 1, price:55000, hora: "8 pm",categoria:"paris", plataforma: "todas" , img: 'https://www.parisciudad.com/fotos/tren-larga-distancia.jpg'},
+    {id: 19, name: "D1 Andorra", cantidad: 1, price:50000, hora: "10 am",categoria:"andorra", plataforma: "todas" , img: 'https://soloski.net/wp-content/uploads/2020/11/tren-cercanias.jpg'},
+    {id: 20, name: "D2 Andorra", cantidad: 1, price:60000, hora: "12 pm",categoria:"andorra", plataforma: "todas" , img: 'https://soloski.net/wp-content/uploads/2020/11/tren-cercanias.jpg'},
+    {id: 21, name: "D3 Andorra", cantidad: 1, price:100000, hora: "2 pm",categoria:"andorra", plataforma: "todas" , img: 'https://soloski.net/wp-content/uploads/2020/11/tren-cercanias.jpg'},
+    {id: 22, name: "D4 Andorra", cantidad: 1, price:100000, hora: "4 pm",categoria:"andorra", plataforma: "todas" , img: 'https://soloski.net/wp-content/uploads/2020/11/tren-cercanias.jpg'},
+    {id: 23, name: "D5 Andorra", cantidad: 1, price:40000, hora: "6 pm",categoria:"andorra", plataforma: "todas" , img: 'https://soloski.net/wp-content/uploads/2020/11/tren-cercanias.jpg'},
+    {id: 24, name: "D6 Andorra", cantidad: 1, price:55000, hora: "8 pm",categoria:"andorra", plataforma: "todas" , img: 'https://soloski.net/wp-content/uploads/2020/11/tren-cercanias.jpg'}
+]
+
+
+
